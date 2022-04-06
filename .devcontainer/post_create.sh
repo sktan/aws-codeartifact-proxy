a0160b3c0e3066594f1994ebbcefc8d64c9a120b
Steven Tan <git@sktan.com>
2022-04-06 13:50:56 +0000

    Update devcontainer to use aws-cli feature

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 
-# Install x86 or ARM version of awscliv2 based on current machine architecture
-if [[ "$(uname -m)" == "x86_64" ]]; then
-  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
-else
-  curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "/tmp/awscliv2.zip"
-fi
-unzip /tmp/awscliv2.zip -d /tmp/awscliv2
-/tmp/awscliv2/aws/install && rm -rf /tmp/awscliv2*
-
 npm install -g aws-cdk
 pip install pre-commit
